93a17e76ee62b1df53082742e67e79d0f232099a
chrisMacPortR <55281081+chrisD91@users.noreply.github.com>
2022-01-28 17:57:29 +0100

    ekg_to_hr : updated docstrings for homogeneity (numpy style) and variables names

--- anesplot/treatrec/ekg_to_hr.py
+++ anesplot/treatrec/ekg_to_hr.py
@@ -48,7 +48,7 @@ typically (copy, paste and execute line by line)
         params = waves.param
         ekg_df = pd.DataFrame(waves.data.wekg)
         ekg_df['wekg_lowpass'] = rec.wf.fix_baseline_wander(ekg_df.wekg,
-                                                waves.param['fs'])
+                                                waves.param['sampling_freq'])
         beat_df = tohr.detect_beats(ekg_df.wekg_lowpass, mult=1)
 
 3. perform the manual adjustments required:
@@ -105,6 +105,7 @@ ____
 """
 
 import os
+from typing import Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -118,23 +119,38 @@ from scipy.interpolate import interp1d
 # TODO shift the ihr of one beat (ie ihr should be on the end of the interval)
 
 #%%
-def detect_beats(ser, fs=300, species="horse", mult=1):
-    """detect the peak locations
 
-    :param pandas.series ser: the data
-    :param integer fs: sampling frequency
-    :param string species: in [horse]
-    :param float mult: correction / 1 for qRs amplitude
-    :returns: df=pandas.Dataframe
+
+def detect_beats(
+    ser: pd.Series, fs: int = 300, species: str = "horse", mult: float = 1
+) -> pd.DataFrame:
+    """
+    detect the peak locations of the beats
+
+    Parameters
+    ----------
+    ser : pd.Series
+        the EKG time series.
+    fs : int, optional (default is 300)
+        sampling frequency.
+    species : str, optional (default is "horse")
+        the species.
+    mult : float, optional (default is 1)
+        correction / 1 for qRs amplitude.
+
+    Returns
+    -------
+    df : pandas.DataFrame
+
     """
 
     df = pd.DataFrame()
     # fs = param.get('fs', 300)
     if species == "horse":
-        height = 1  # mini
+        height = 1.0  # mini
         hr_max = 120  # bpm
         distance = fs * 60 / hr_max  # sec
-        prominence = 1  # mini/surrounding
+        prominence = 1.0  # mini/surrounding
         #    width=(2,15)
         #    plateau_size= 1
     else:
@@ -156,19 +172,34 @@ def detect_beats(ser, fs=300, species="horse", mult=1):
     return df
 
 
-def plot_beats(ecg, beats):
-    """plot ecg waveform + beat location"""
+# ekg_df.wekg_lowpass, beat_df)
+def plot_beats(ekgdf: pd.DataFrame, beatdf: pd.DataFrame) -> plt.Figure:
+    """
+    plot beat location on ekg display and rr values over time
+
+    Parameters
+    ----------
+    ekgdf : pd.DataFrame.
+        waves data (wekg & wekg_lowpass)
+    beatdf : pd.DataFrame
+        the location of the beats (columns used are [p_loc and y_loc]).
+
+    Returns
+    -------
+    fig : matplotlib.pyplot.Figure
+
+    """
 
     fig = plt.figure(figsize=(13, 5))
     fig.suptitle("verify the accuracy of the beat detection")
     ax0 = fig.add_subplot(211)
-    ax0.plot(ecg.values, label="ekg")
+    ax0.plot(ekgdf.values, label="ekg")
     ax0.set_ylabel("ekg (mV)")
     ax0.set_xlabel("pt value")
-    ax0.plot(beats.p_loc, beats.y_loc, "o", color="orange", label="R")
+    ax0.plot(beatdf.p_loc, beatdf.y_loc, "o", color="orange", label="R")
 
     ax1 = fig.add_subplot(212, sharex=ax0)
-    ax1.plot(beats.p_loc[:-1], np.diff(beats.p_loc), "r-", label="rr")
+    ax1.plot(beatdf.p_loc[:-1], np.diff(beatdf.p_loc), "r-", label="rr")
     ax1.set_ylabel("rr (pt value)")
     ax1.set_xlabel("pt value")
     fig.legend()
@@ -187,18 +218,37 @@ def plot_beats(ecg, beats):
     return fig
 
 
-def append_beat(beatdf, ekgdf, tochange_df, fig, lim=None, yscale=1):
-    """locate the beat in the figure, append to a dataframe['toAppend']
+def append_beat(
+    beatdf: pd.DataFrame,
+    ekgdf: pd.DataFrame,
+    tochangedf: pd.DataFrame,
+    fig: plt.Figure,
+    lim: Tuple = None,
+    yscale: float = 1,
+) -> pd.DataFrame:
+    """
+    append a beat coordonate from the figure to the tochangedf['toAppend']
 
-    :param pandas.Dataframe beatdf: contains the point based location (p_locs)
-    :param pandas dataframe ekgdf: contains the wave recording ((wekg_lowpass)
-    :param pandas.Dataframa tochange_df: to store the beats toAppend or toRemove
-    :param pyplot.Figure fig: figure to find time limits
-    :param integer lim: ptBasedLim optional to give it manually
-    :param float  yscale: amplitude mutliplication factor for detection (default=1)
+    Parameters
+    ----------
+    beatdf : pd.Dataframe
+        beat position (point based location : p_locs)
+    ekgdf : pd.Dataframe
+        waves data (wekg_lowpass).
+    tochangedf : pd.Dataframe
+        the beat to add or remove (point based toAppend & toRemove)
+    fig : plt.Figure
+        the figure to get the location.
+    lim : TYPE, optional (default is None)
+        ptBasedLim optional to give it manually
+    yscale : TYPE, optional (default is 1)
+        amplitude mutliplication factor for detection.
 
-    :returns: tochange_df: incremented changedf (pt location)
-    :rtype: pandasDataframe
+    Returns
+    -------
+    tochangedf : pd.DataFrame
+        incremented changedf (pt location).
+
 
     methods :
 
@@ -210,7 +260,7 @@ def append_beat(beatdf, ekgdf, tochange_df, fig, lim=None, yscale=1):
             and zoom to observe only a negative peak
 
         2.: call the function:
-            >>> to_change_df = remove_beat(beatdf, ekgdf, tochange_df, fig)
+            >>> to_change_df = remove_beat(beatdf, ekgdf, tochangedf, fig)
             -> the beat parameters will be added the dataFrame
 
         .in the end of the manual check, update the beat_df
@@ -230,7 +280,7 @@ def append_beat(beatdf, ekgdf, tochange_df, fig, lim=None, yscale=1):
     onepoint_beatdf["action"] = "append"
     if len(onepoint_beatdf) < 1:
         print("no beat founded")
-        return tochange_df
+        return tochangedf
     found_loc = onepoint_beatdf.p_loc.values[0]
     y_loc = onepoint_beatdf.y_loc.values[0]
     # reassign the pt value
@@ -242,14 +292,43 @@ def append_beat(beatdf, ekgdf, tochange_df, fig, lim=None, yscale=1):
     onepoint_beatdf.p_loc = p_loc
     onepoint_beatdf.left_bases += p_loc - found_loc
     onepoint_beatdf.right_bases.values[0] += p_loc - found_loc
-    # insert in the tochange_df
+    # insert in the tochangedf
     # beatdf = beatdf.drop_duplicates('p_loc')
-    tochange_df = tochange_df.append(onepoint_beatdf)
-    return tochange_df
+    tochangedf = tochangedf.append(onepoint_beatdf)
+    return tochangedf
 
 
-def remove_beat(beatdf, ekgdf, tochange_df, fig, lim=None):
-    """locate the beat in the figure, append to a dataframe['toRemove']
+def remove_beat(
+    beatdf: pd.DataFrame,
+    ekgdf: pd.DataFrame,
+    tochangedf: pd.DataFrame,
+    fig: plt.figure,
+    lim: Tuple = None,
+) -> pd.DataFrame:
+    """
+    remove a beat coordinate from the figure to the tochangedf['toRemove']
+
+    Parameters
+    ----------
+    beatdf : pd.Dataframe
+        beat position (point based location : p_locs)
+    ekgdf : pd.Dataframe
+        waves data (wekg_lowpass).
+    tochangedf : pd.Dataframe
+        the beat to add or remove (point based toAppend & toRemove)
+    fig : plt.Figure
+         the figure to get the location.
+    lim : TYPE, optional (default is None)
+         ptBasedLim optional to give it manually
+    yscale : TYPE, optional (default is 1)
+        amplitude mutliplication factor for detection.
+
+    Returns
+    -------
+    tochangedf : pd.DataFrame
+    incremented changedf (pt location).
+
+    locate the beat in the figure, append to a dataframe['toRemove']
 
     0.: if not present build a dataframe:
         >>> to_change_df = pd.DataFrame(columns=['toAppend', 'toRemove'])
@@ -258,7 +337,7 @@ def remove_beat(beatdf, ekgdf, tochange_df, fig, lim=None):
         and zoom to observe only a negative peak
 
     2.: call the function:::
-        >>> to_change_df = remove_beat(beatdf, ekgdf, tochange_df, fig)
+        >>> to_change_df = remove_beat(beatdf, ekgdf, tochangedf, fig)
         -> the beat parameters will be added the dataFrame
 
     .(in the end of the manual check, update the beat_df
@@ -278,7 +357,7 @@ def remove_beat(beatdf, ekgdf, tochange_df, fig, lim=None):
     ptloc = position.values  # pt values of the peak
     if len(ptloc) > 1:
         print("several beats detected")
-        return tochange_df
+        return tochangedf
     pos = int(ptloc[0])  # array -> value
     # mark on the graph
     p_loc, y_loc = beatdf.loc[iloc, ["p_loc", "y_loc"]]
@@ -288,17 +367,24 @@ def remove_beat(beatdf, ekgdf, tochange_df, fig, lim=None):
     # mark to remove
     onepoint_beatdf = beatdf.loc[iloc].copy()
     onepoint_beatdf["action"] = "remove"
-    tochange_df = tochange_df.append(onepoint_beatdf, ignore_index=True)
+    tochangedf = tochangedf.append(onepoint_beatdf, ignore_index=True)
     # beatdf.loc[pos, ['y_loc']] = np.NaN
     print("position is ", pos)
-    return tochange_df
+    return tochangedf
 
 
 # TODO append the missing R in case of BAV2
 
 
-def save_beats(beatdf, tochangedf, savename="", dirpath=None):
-    """save the beats locations as csv and hd5 file
+def save_beats(
+    beatdf: pd.DataFrame,
+    tochangedf: pd.DataFrame,
+    savename: str = "",
+    dirpath: str = None,
+):
+
+    """
+    save the beats locations as csv and hd5 file
 
     parameters
     ----------
@@ -330,10 +416,33 @@ def save_beats(beatdf, tochangedf, savename="", dirpath=None):
 #%% apply changes to the beatdf
 
 
-def update_beat_df(beatdf, tochangedf, path_to_file="", from_file=False):
-    """implement in the beat location the manual corrections
-    fromFile = True force the disk loading of the dataframes
+def update_beat_df(
+    beatdf: pd.DataFrame,
+    tochangedf: pd.DataFrame,
+    path_to_file: str = "",
+    from_file: bool = False,
+):
     """
+    implement in the beat location the manual corrections
+
+    Parameters
+    ----------
+    beatdf : pd.DataFrame
+        beat position (point based location : p_locs)
+    tochangedf : pd.DataFrame
+        the beat to add or remove (point based toAppend & toRemove)
+    path_to_file : str, optional (default is "")
+        dirpath to the saved file.
+    from_file : bool, optional (default is False)
+        fromFile = True force the disk loading of the dataframes
+
+    Returns
+    -------
+    beatdf : pd.DataFrame
+        updated beat position
+
+    """
+
     if from_file:
         name = os.path.join(path_to_file, "beatDf.csv")
         try:
@@ -361,21 +470,21 @@ def update_beat_df(beatdf, tochangedf, path_to_file="", from_file=False):
 # beat_df = update_beat_df(beat_df, to_change_df)
 
 #%% =========================================
-def compute_rr(beatdf, fs=None):
+def compute_rr(beatdf: pd.DataFrame, fs: int = None) -> pd.DataFrame:
     """
     compute rr intervals (from pt to time)
 
-    parameters
+    Parameters
     ----------
     beatdf : pd.DataFrame
-        with 'p_loc'
-    fs : integer
-        sampling frequency
+        beat position (point based location : p_locs)
+    fs : int, optional (default is None -> 300)
+        the sampling frequency
 
-    returns
+    Returns
     -------
-    pd.DataFrame
-        with:
+    beatdf : pd.DataFrame
+        beat position updated with rrvalues:
         'rr' =  rr duration
         'rrDiff' = rrVariation
         'rrSqDiff' = rrVariation^2
@@ -399,21 +508,22 @@ def compute_rr(beatdf, fs=None):
     return beatdf
 
 
-def interpolate_rr(beatdf, kind=None):
+def interpolate_rr(beatdf: pd.DataFrame, kind: str = None) -> pd.DataFrame:
     """
     interpolate the beat_df (pt -> time values)
 
-    parameters
+    Parameters
     ----------
-    beatDf: pd.Dataframe
-    kind : str
-        'linear' or 'cubic'(default)
+    beatdf : pd.DataFrame
+        beat position (point based location : p_locs).
+    kind : str, optional (default is None -> "cubic")
+        interpolation (in ['linear', 'cubic']
 
-    returns
+    Returns
     -------
-    pdDatatrame with evenly spaced data
-        'espts' = evenly spaced points
-        'rrInterpol' = interpolated rr
+    ahr_df : pd.DataFrame
+        evenly spaced data with 'espts' = evenly spaced points & 'rrInterpol' = interpolated rr
+
     """
 
     if kind is None:
@@ -446,17 +556,25 @@ def interpolate_rr(beatdf, kind=None):
     return ahr_df
 
 
-def plot_rr(ahr_df, param, HR=False):
+def plot_rr(ahr_df: pd.DataFrame, param: dict, HR: bool = False) -> plt.Figure:
     """
     plot RR vs pt values + rrSqDiff
 
-    parameters:
-        hr_df = pdDataFrame
-        params : dict
-            containing 'fs' as key
+    Parameters
+    ----------
+    ahr_df : pd.DataFrame
+        DESCRIPTION.
+    param : dict
+        containing 'sampling_freq' as key.
+    HR : bool, optional (default is False)
+        to display HR instead of rr
+    Returns
+    -------
+    fig : plt.Figure
+
     """
 
-    fs = param["fs"]
+    fs = param["sampling_freq"]
 
     fig = plt.figure(figsize=(8, 4))
     ax = fig.add_subplot(211)
@@ -498,16 +616,31 @@ def plot_rr(ahr_df, param, HR=False):
 # TOTO = correct wave.datetime (multiple repetitions of the same value)
 
 
-def append_rr_and_ihr_to_wave(wave, ahrdf):
-    """append rr and ihr to the waves based on pt value (ie index)"""
+def append_rr_and_ihr_to_wave(ekgdf: pd.DataFrame, ahrdf: pd.DataFrame) -> pd.DataFrame:
+    """
+    append rr and ihr to the waves based on pt value (ie index)
 
-    df = pd.concat([wave, ahrdf.set_index("espts")], axis=1)
+    Parameters
+    ----------
+    ekgdf : pd.DataFrame
+        waves data
+    ahrdf : pd.DataFrame
+        evenly spaced interpolated data.
+
+    Returns
+    -------
+    df : pd.DataFrame
+        added iHR to ekgdf.
+
+    """
+
+    df = pd.concat([ekgdf, ahrdf.set_index("espts")], axis=1)
     df["ihr"] = 1 / df.rrInterpol * 60 * 1000
     print("added instantaneous heart rate to a Wave dataframe")
     return df
 
 
-def plot_agreement(trenddf):
+def plot_agreement(trenddf: pd.DataFrame):
     """plot ip1HR & ihr to check agreement"""
 
     fig = plt.figure()
@@ -519,8 +652,27 @@ def plot_agreement(trenddf):
     return fig
 
 
-def append_ihr_to_trend(trenddf, wavedf, ekgdf):
-    """append 'ihr' (instataneous heart rate) to the trends"""
+def append_ihr_to_trend(
+    trenddf: pd.DataFrame, wavedf: pd.DataFrame, ekgdf: pd.DataFrame
+) -> pd.DataFrame:
+    """
+    append 'ihr' (instataneous heart rate) to the trends
+
+    Parameters
+    ----------
+    trenddf : pd.DataFrame
+        DESCRIPTION.
+    wavedf : pd.DataFrame
+        DESCRIPTION.
+    ekgdf : pd.DataFrame
+        DESCRIPTION.
+
+    Returns
+    -------
+    trenddf : TYPE
+        DESCRIPTION.
+
+    """
 
     # build a new index
     ratio = len(wavedf) / len(trenddf)
@@ -538,20 +690,23 @@ def append_ihr_to_trend(trenddf, wavedf, ekgdf):
     return trenddf
 
 
-def save_trends_data(trenddf, savename="", dirpath="data"):
+def save_trends_data(trenddf: pd.DataFrame, savename: str = "", dirpath: str = "data"):
     """
-    save the trends data to a csv and hd5 file, including an ihr column
+    save the trends data to a csv and hd5 file, including an ihr column (key='trends_data')
 
-    parameters
+    Parameters
     ----------
-    trenddf : pd.dataframes
-    savename : str
-    dirpath : str
-        path to save in (default= current working directory)
+    trenddf : pd.DataFrame
+        the (updated) trend recording.
+    savename : str, optional (default is "" ->  _trendData)
+        (short) file name to use
+    dirpath : str, optional
+        DESCRIPTION. The default is cwd.
 
-    output
-    ------
-    hdf file, key='trends_data'
+    Returns
+    -------
+    None.
+
     """
 
     if dirpath is None:
@@ -567,19 +722,23 @@ def save_trends_data(trenddf, savename="", dirpath="data"):
     trenddf.to_hdf(fullname + ".hdf", mode="w", key="trends_data")
 
 
-def save_waves_data(wavedf, savename="", dirpath="data"):
+def save_waves_data(wavedf: pd.DataFrame, savename: str = "", dirpath: str = "data"):
     """
-    save the trends data to a hd5 file, including an ihr column
+    save the waves data to a csv and hd5 file, including an ihr column (key='waves_data')
 
-    parameters
+    Parameters
     ----------
-    trenddf : pd.dataframes
-    savename : str
-        dirpath : path to save in (default='data')
+    wavedf : pd.DataFrame
+        the (updated) trend recording.
+    savename : str, optional (default is "" ->  _trendData)
+        (short) file name to use
+    dirpath : str, optional
+        DESCRIPTION. The default is cwd.
 
-    output
-    ------
-    hdf_file, key='waves_data'
+    Returns
+    -------
+    None.
+
     """
 
     if dirpath is None:
