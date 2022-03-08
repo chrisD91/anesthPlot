7c9cd60cb9077bf5acea315f8117114e0a793b70
chrisMacPortR <55281081+chrisD91@users.noreply.github.com>
2022-03-08 12:38:15 +0100

    a first export working version

--- anesplot/loadrec/export_reload.py
+++ anesplot/loadrec/export_reload.py
@@ -11,24 +11,67 @@ import os
 
 import pandas as pd
 
-exp = os.path.join(os.path.expanduser("~"), "toPlay", "export.hdf")
 
-mtrends.data.to_hdf(exp, key="mtrend_data")
-pd.DataFrame.from_dict({k: [str(v)] for k, v in mtrends.header.items()}).T.to_hdf(
-    exp, key="mtrend_header"
-)
-pd.DataFrame.from_dict({k: [str(v)] for k, v in mtrends.param.items()}).T.to_hdf(
-    exp, key="mtrend_param"
-)
-# filename
+def export_records(savename, mtrend=None, ttrend=None, mwave=None):
+    """
+    export the recordings in an hdf file
+    keys are
+        mtrends_data, mtrends_header, mtrends_param
+        ttrends_data, ttrends_header, ttrends_param
+        mwaves_data, mwaves_header, mwaves_param
 
-ttrends.data.to_hdf(exp, key="ttrend_data")
-pd.DataFrame.from_dict({k: [str(v)] for k, v in mtrends.header.items()}).T.to_hdf(
-    exp, key="trend_header"
-)
-pd.DataFrame.from_dict({k: [str(v)] for k, v in mtrends.param.items()}).T.to_hdf(
-    exp, key="trend_param"
-)
+    Parameters
+    ----------
+    savename : str
+        the savename to use.
+    mtrend : MonitorTrend object, optional (default is None)
+        The Monitor recording
+    ttrend : TaphoniusTrend, optional (default is None)
+        the taphonius recording
+    mwave : MonitorWave, optional (default is None)
+        the wave recording
 
-duplicates = {_ for _ in cols if cols.count(_) > 1}
-print(f"{duplicates=}")
+    Returns
+    -------
+    None.
+
+    """
+
+    # monitor trends
+    if mtrend:
+        mtrend.data.to_hdf(savename, key="mtrends_data")
+        pd.DataFrame.from_dict(
+            {k: [str(v)] for k, v in mtrend.header.items()}
+        ).T.to_hdf(savename, key="mtrends_header")
+        pd.DataFrame.from_dict({k: [str(v)] for k, v in mtrend.param.items()}).T.to_hdf(
+            savename, key="mtrends_param"
+        )
+    # filename
+
+    # taph trends
+    if ttrend:
+        ttrend.data.to_hdf(savename, key="ttrends_data")
+        pd.DataFrame.from_dict(
+            {k: [str(v)] for k, v in ttrend.header.items()}
+        ).T.to_hdf(savename, key="ttrends_header")
+        pd.DataFrame.from_dict({k: [str(v)] for k, v in ttrend.param.items()}).T.to_hdf(
+            savename, key="ttrends_param"
+        )
+
+    # waves
+    if mwave:
+        mwave.data.to_hdf(savename, key="mwaves_data")
+        pd.DataFrame.from_dict({k: [str(v)] for k, v in mwave.header.items()}).T.to_hdf(
+            savename, key="mwaves_header"
+        )
+        pd.DataFrame.from_dict({k: [str(v)] for k, v in mwave.param.items()}).T.to_hdf(
+            savename, key="mwaves_param"
+        )
+
+
+save_name = os.path.join(os.path.expanduser("~"), "toPlay", "export.hdf")
+export_records(save_name, mtrend=mtrends, ttrend=ttrends, mwave=mwaves)
+
+
+# duplicates = {_ for _ in cols if cols.count(_) > 1}
+# print(f"{duplicates=}")
