03f2d5ef6a49101089f93355d68d29341e8c1c85
chrisMacPortR <55281081+chrisD91@users.noreply.github.com>
2022-05-06 12:47:59 +0200

    config: updated the module docstring

--- anesplot/config/__init__.py
+++ anesplot/config/__init__.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+Created on Fri May  6 12:40:23 2022
 
-# from anesthPlot.config import build_recordrc
-# from anesthPlot.config import load_recordrc
+the configuration load/build modules
 
-# from . import build_recordrc
-# from . import load_recordrc
-
-# import config.build_recordrc
-# import config.load_recordrc
+@author: cdesbois
+"""
