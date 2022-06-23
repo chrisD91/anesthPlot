70fa1aea64f9ea54c86a6a69206508b7910a29a3
chrisMacPortR <55281081+chrisD91@users.noreply.github.com>
2022-06-23 16:24:36 +0200

    plot moved the constants to an external file

--- anesplot/plot/t_axplot.py
+++ anesplot/plot/t_axplot.py
@@ -9,277 +9,12 @@ trend_axis_plot :
     and append the plot to the provided axes
 """
 from types import SimpleNamespace as sn
-from typing import Any
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-
-def get_cte(key: str = "default") -> dict[str, Any]:
-    """
-    Build a dico containing the drawing constants.
-
-    Parameters
-    ----------
-    key : str, optional (default is 'default')
-        key for the display
-
-    Returns
-    -------
-    cte : dict
-        the 'constants' to be used to build the plot.
-
-    """
-    dicos: dict[str, dict[str, Any]] = {
-        "default": dict(
-            key="",
-            label="",
-            color="blue",
-            edgecolor="blue",
-            unit="",
-            goals=[],
-            traces=[],
-            ylims=[None, None],
-        ),
-        "o2": dict(
-            key="o2",
-            label="oxygen",
-            color="tab:green",
-            fillalpha=0.2,
-            edgecolor="tab:green",
-            unit="%",
-            goals=[30, 50],
-            traces=[],
-            ylims=[20, 80],
-        ),
-        "co2": dict(
-            key="co2",
-            label="end tidal co2",
-            color="tab:blue",
-            fillalpha=0.2,
-            edgecolor="tab:blue",
-            unit="mmHg",
-            goals=[35, 45],
-            traces=[],
-            ylims=[30, 150],
-        ),
-        "co2rr": dict(
-            key="co2rr",
-            label="co2 respirationRate",
-            color="tab:blue",
-            fillalpha=0.2,
-            edgecolor="tab:blue",
-            unit="mmHg",
-            goals=[5, 15],
-            traces=[],
-            ylims=[6, 10],
-            style="--",
-        ),
-        "iso": dict(
-            key="iso",
-            label="isoflurane",
-            color="tab:purple",
-            fillalpha=0.2,
-            edgecolor="tab:purple",
-            unit="%",
-            goals=[1.2, 1.4],
-            traces=[],
-            ylims=[0, 2],
-        ),
-        "tvinsp": dict(
-            # datex
-            key="tvinsp",
-            label="tidalVolume insp",
-            color="tab:orange",
-            fillalpha=0.2,
-            style="-",
-            edgecolor="tab:orange",
-            unit="l",
-            goals=[5, 6],
-            traces=[],
-            ylims=[0, 7],
-            calib=187,
-        ),
-        "tvspont": dict(
-            # taph
-            key="tvspont",
-            label="tidalVolume spont",
-            color="tab:olive",
-            fillalpha=0.2,
-            style="-",
-            edgecolor="tab:olive",
-            unit="l",
-            goals=[5, 6],
-            traces=[],
-            ylims=[0, 7],
-        ),
-        "tvcontrol": dict(
-            # taph
-            key="tvcontrol",
-            label="tidalVolume control",
-            color="tab:orange",
-            fillalpha=0.2,
-            style="-",
-            edgecolor="tab:orange",
-            unit="l",
-            goals=[5, 6],
-            traces=[],
-            ylims=[0, 7],
-        ),
-        "minvol": dict(
-            # taph
-            key="minvol",
-            label="minuteVolume",
-            color="tab:olive",
-            fillalpha=0.2,
-            style="-",
-            edgecolor="tab:olive",
-            unit="uncalibrated",
-            goals=[5, 6],
-            traces=[],
-            ylims=[0, 7],
-        ),
-        "pPeak": dict(
-            # taph
-            key="tvcontrol",
-            label="tidalVolume control",
-            color="tab:red",
-            fillalpha=0.2,
-            style="-",
-            edgecolor="tab:red",
-            unit="l",
-            goals=[5, 6],
-            traces=[],
-            ylims=[0, 7],
-        ),
-        "peep": dict(
-            # taph
-            key="tvcontrol",
-            label="tidalVolume control",
-            color="tab:red",
-            fillalpha=0.2,
-            style="-",
-            edgecolor="tab:red",
-            unit="l",
-            goals=[5, 6],
-            traces=[],
-            ylims=[0, 7],
-        ),
-        "pPlat": dict(
-            # taph
-            key="tvcontrol",
-            label="tidalVolume control",
-            color="tab:red",
-            fillalpha=0.2,
-            style="-",
-            edgecolor="tab:red",
-            unit="cmH20",
-            goals=[10, 25],
-            traces=[],
-            ylims=[0, 30],
-        ),
-        "setrr": dict(
-            # taph
-            key="setrr",
-            label="set_respiratoryRate",
-            color="k",
-            fillalpha=1,
-            style=":",
-            edgecolor="k",
-            unit="rpm",
-            goals=[5, 10],
-            traces=[],
-            ylims=[0, 15],
-        ),
-        "settv": dict(
-            # taph
-            key="settv",
-            label="set_tidalVolume",
-            color="k",
-            fillalpha=1,
-            style=":",
-            edgecolor="tab:orange",
-            unit="l",
-            goals=[5, 6],
-            traces=[],
-            ylims=[0, 7],
-        ),
-        "setpeep": dict(
-            # taph
-            key="setpeep",
-            label="set_peep",
-            color="k",
-            fillalpha=1,
-            style=":",
-            edgecolor="tab:red",
-            unit="l",
-            goals=[5, 6],
-            traces=[],
-            ylims=[0, 7],
-        ),
-        "sat": dict(
-            key="sat",
-            label="sp02",
-            color="tab:red",
-            tracealpha=0.8,
-            edgecolor="tab:red",
-            unit="%",
-            goals=[90, 100],
-            traces=[],
-            ylims=[60, 100],
-        ),
-        "sathr": dict(
-            key="sathr",
-            label="spO2 heart rate",
-            color="tab:gray",
-            edgecolor="tab:gray",
-            style=":",
-            unit="bpm",
-            goals=[30, 50],
-            traces=[],
-            ylims=[20, 100],
-        ),
-        "hr": dict(
-            key="hr",
-            label="heart rate",
-            color="tab:gray",
-            edgecolor="tab:gray",
-            unit="bpm",
-            goals=[30, 50],
-            traces=[],
-            ylims=[20, 100],
-        ),
-        "ip1": dict(
-            key="ip1",
-            label="arterial pressure",
-            color="tab:red",
-            edgecolor="red",
-            unit="mmHg",
-            goals=[70, 80],
-            traces=["ip1" + _ for _ in ["m", "d", "s"]],
-            ylims=[30, 150],
-            fillalpha=0.5,
-        ),
-        "ip2": dict(
-            key="ip2",
-            label="venous pressure",
-            color="tab:blue",
-            edgecolor="blue",
-            unit="mmHg",
-            goals=[10, 20],
-            traces=["ip2" + _ for _ in ["m", "d", "s"]],
-            ylims=[0, 20],
-            fillalpha=0.4,
-        ),
-    }
-    if key in dicos:
-        dico = dicos[key]
-        out_dico = dicos["default"] | dico
-    else:
-        print(f"no cts defined for {key}, used default")
-        out_dico = dicos["default"]
-    return out_dico
+from anesplot.plot.cts_plot import cts_dico
 
 
 def axplot_hist(ax: plt.axes, ser: pd.Series, key: str = "ip1") -> None:
@@ -303,7 +38,7 @@ def axplot_hist(ax: plt.axes, ser: pd.Series, key: str = "ip1") -> None:
     defined = ["ip1", "ip2", "hr", "co2", "iso"]
     if key not in defined:
         print(f"key should be in {defined} ({key} was used)")
-    cts = sn(**get_cte(key))  # get the drawing constants
+    cts = sn(**(cts_dico[key]))  # get the drawing constants
     ax.set_title(cts.label, color=cts.color)
     if len(ser) > 0:
         ax.hist(
@@ -350,11 +85,11 @@ def axplot_ventiltidal(ax: plt.axes, df: pd.DataFrame) -> None:
         # calib = ttrend.data.tvInsp.mean() / taph_trend.data.tv.mean()
         # calib = 187
         # ax.plot(df.tvInsp / calib, color="tab:orange", linewidth=2, label="tvInsp")
-        cts = sn(**get_cte("tvinsp"))
+        cts = sn(**cts_dico["tvinsp"])
         calib = cts.calib
         ax.plot(df.tvInsp / calib, color=cts.color, linewidth=2, label=cts.label)
     elif "tv_spont" in df.columns:  # taph
-        cts = sn(**get_cte("tvspont"))
+        cts = sn(**cts_dico["tvspont"])
         ax.plot(
             df.tv_spont,
             color=cts.color,
@@ -363,9 +98,9 @@ def axplot_ventiltidal(ax: plt.axes, df: pd.DataFrame) -> None:
             label=cts.label,
         )
         try:
-            cts = sn(**get_cte("tvcontrol"))
+            cts = sn(**cts_dico["tvcontrol"])
             ax.plot(df.tv_control, color=cts.color, linewidth=2, label=cts.label)
-            cts = sn(**get_cte("settv"))
+            cts = sn(**cts_dico["settv"])
             ax.plot(
                 df.set_tv,
                 color=cts.color,
@@ -424,7 +159,7 @@ def axplot_ventilpressure(ax: plt.axes, df: pd.DataFrame) -> None:
 
     if keys:
         for key in keys:
-            cts = sn(**get_cte(key))
+            cts = sn(**cts_dico[key])
             ax.plot(
                 df[key],
                 color=cts.color,
@@ -440,7 +175,7 @@ def axplot_ventilpressure(ax: plt.axes, df: pd.DataFrame) -> None:
             alpha=0.1,
         )
         try:
-            cts = sn(**get_cte("setpeep"))
+            cts = sn(**cts_dico["setpeep"])
             ax.plot(
                 df.set_peep,
                 color=cts.color,
@@ -469,9 +204,9 @@ def axplot_minvol_rr(ax: plt.axes, df: pd.DataFrame) -> None:
 
     """
     ax.set_ylabel("'MinVol' & RR")
-    minvol = sn(**get_cte("minvol"))
+    minvol = sn(**cts_dico["minvol"])
     if "co2_rr" in df.columns:
-        co2rr = sn(**get_cte("co2rr"))
+        co2rr = sn(**cts_dico["co2rr"])
         ax.plot(
             df.co2_rr,
             color=co2rr.color,
@@ -482,7 +217,7 @@ def axplot_minvol_rr(ax: plt.axes, df: pd.DataFrame) -> None:
     if "minVexp" in df.columns:  # monitor
         ax.plot(df.minVexp, color=minvol.color, linewidth=2, label=minvol.label)
     if "set_rr" in df.columns:  # taph
-        setrr = sn(**get_cte("setrr"))
+        setrr = sn(**cts_dico["setrr"])
         ax.plot(
             df.set_rr,
             color=setrr.color,
@@ -518,7 +253,7 @@ def axplot_etco2(ax: plt.axes, df: pd.DataFrame) -> None:
     None.
 
     """
-    co2 = sn(**get_cte("co2"))  # get the drawing constants
+    co2 = sn(**cts_dico["co2"])  # get the drawing constants
     ax.set_ylabel(co2.label)
     try:
         ax.plot(df.co2exp, color=co2.color, linewidth=2, linestyle="-")
@@ -560,7 +295,7 @@ def axplot_iso(ax: plt.axes, df: pd.DataFrame) -> None:
     None.
 
     """
-    iso = sn(**get_cte("iso"))  # get the drawing constants
+    iso = sn(**cts_dico["iso"])  # get the drawing constants
     ax.set_ylabel(iso.label)
     try:
         ax.plot(df.aaExp, color=iso.color, linewidth=2, linestyle="-")
@@ -603,7 +338,7 @@ def axplot_o2(ax: plt.axes, df: pd.DataFrame) -> None:
     None.
 
     """
-    oxy = sn(**get_cte("o2"))  # get the drawing constants
+    oxy = sn(**cts_dico["o2"])  # get the drawing constants
     ax.set_ylabel(oxy.label)
     try:
         ax.plot(df.o2insp, color=oxy.color, linewidth=2, linestyle="-")
@@ -649,7 +384,7 @@ def axplot_arterialpressure(ax: plt.axes, df: pd.DataFrame, key: str = "ip1") ->
 
     """
     if key in ["ip1", "ip2"]:
-        press = sn(**get_cte(key))  # get the drawing constants
+        press = sn(**cts_dico[key])  # get the drawing constants
     else:
         txt = "key should be in [ip1, ip2]"
         ax.text(0.5, 0.5, txt)
@@ -684,7 +419,7 @@ def axplot_sat(ax: plt.axes, df: pd.DataFrame) -> None:
     None
 
     """
-    sat = sn(**get_cte("sat"))  # get the drawing constants
+    sat = sn(**cts_dico["sat"])  # get the drawing constants
     ax.plot(df.sat, color=sat.color, label=sat.label, linewidth=2)
     ax.set_ylabel(sat.label)
     ax.set_ylim(*sat.ylims)
@@ -713,7 +448,7 @@ def axplot_hr(ax: plt.axes, df: pd.DataFrame) -> None:
     None
 
     """
-    hrate = sn(**get_cte("hr"))  # get the drawing constants
+    hrate = sn(**cts_dico["hr"])  # get the drawing constants
     ax.plot(df.hr, color=hrate.color, label=hrate.label, linewidth=2, linestyle=":")
     ax.set_ylabel(hrate.label)
     ax.set_ylim(*hrate.ylims)
@@ -735,7 +470,7 @@ def axplot_sathr(ax: plt.axes, df: pd.DataFrame) -> None:
     None
 
     """
-    sathrate = sn(**get_cte("sathr"))  # get the drawing constants
+    sathrate = sn(**cts_dico["sathr"])  # get the drawing constants
     ax.plot(
         df.spo2Hr,
         color=sathrate.color,
