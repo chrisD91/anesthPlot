9937ab56ae9cff4400d1a330f8c52622a2f9c006
chrisMacPortR <55281081+chrisD91@users.noreply.github.com>
2022-09-28 13:12:51 +0200

    working version of build_recordrc.py : all reference to qt dialog have been removed, internal dialogs module has been used, code has been cleaned

--- anesplot/config/build_recordrc.py
+++ anesplot/config/build_recordrc.py
@@ -11,84 +11,39 @@ build a 'recordRc.yaml' configuration file at the root of anesplot
 
 
 import os
-
-# import sys
 from typing import Any
-
 import yaml  # type: ignore
-
-# from PyQt5 import QtCore
-# from PyQt5.QtWidgets import QApplication, QDialog, QFileDialog
 
 import anesplot.loadrec.dialogs as dlg
 
 # %%
 
 
-# def filedialog(
-#     kind: str = "",
-#     directory: Optional[str] = None,
-#     for_open: bool = True,
-#     fmt: str = "",
-#     is_folder: bool = False,
-# ) -> Union[str, list[str]]:
-#     """
-#     General dialog function.
+def get_config_files() -> tuple[str, str]:
+    """
+    Find the path for config files.
 
-#     Parameters
-#     ----------
-#     kind : str, optional (default is "")
-#         displayed in the dialog title
-#     directory : str, optional (default is os.path.dirname(__file__))
-#         the directory to start with
-#     for_open : bool, optional (default is True)
-#     fmt : str, optional (default is "")
-#     is_folder : bool, optional (default is False)
+    Returns
+    -------
+    main_dir : TYPE
+        base directory for the package 'ie contains record_main.py.
+    cfg_filename : TYPE
+        fullname of the configuration file.
 
-#     Returns
-#     -------
-#     str
-#         the selected path.
-#     """
-#     label = "select the folder for " + kind
-#     options = QFileDialog.Options()
-#     #    options |= QFileDialog.DontUseNativeDialog
-#     #    options |= QFileDialog.DontUseCustomDirectoryIcons
-#     dialog = QFileDialog(caption=label)
-#     dialog.setOptions(options)
+    """
 
-#     dialog.setFilter(dialog.filter() | QtCore.QDir.Hidden)
+    def find_file(name: str, path: str) -> str:
+        """Find file."""
+        filename = ""
+        for root, dirs, files in os.walk(path):
+            if name in files:
+                filename = os.path.join(root, name)
+        return filename
 
-#     # ARE WE TALKING ABOUT FILES OR FOLDERS
-#     if is_folder:
-#         dialog.setFileMode(QFileDialog.DirectoryOnly)
-#     else:
-#         dialog.setFileMode(QFileDialog.AnyFile)
-#     # OPENING OR SAVING
-#     dialog.setAcceptMode(QFileDialog.AcceptOpen) if for_open else dialog.setAcceptMode(
-#         QFileDialog.AcceptSave
-#     )
-
-#     # SET FORMAT, IF SPECIFIED
-#     if fmt != "" and is_folder is False:
-#         dialog.setDefaultSuffix(fmt)
-#         dialog.setNameFilters([f"{fmt} (*.{fmt})"])
-
-#     # SET THE STARTING DIRECTORY
-#     if directory != "":
-#         dialog.setDirectory(str(directory))
-#     else:
-#         try:
-#             __file__
-#             dialog.setDirectory(os.path.dirname(__file__))
-#         except NameError:
-#             dialog.setDirectory(os.getcwd())
-
-#     if dialog.exec_() == QDialog.Accepted:
-#         path = dialog.selectedFiles()[0]  # returns a list
-#     else:
-#         path = ""
-#     return str(path)
+    main_file = find_file(name="record_main.py", path=os.getcwd())
+    main_dir = os.path.dirname(main_file)
+    cfg_filename = find_file(name="recordRc.yaml", path=os.getcwd())
+    return main_dir, cfg_filename
 
 
 def read_config(cfg_filename: str) -> dict[str, Any]:
@@ -106,20 +61,6 @@ def read_config(cfg_filename: str) -> dict[str, Any]:
         a dictionary to the defined paths.
 
     """
-    # locate
-    # try:
-    #     # for external call
-    #     # NB __file__ is supposed to
-    #     # "always give you the path to the current file",
-    #     # and sys.argv[0] is supposed to
-    #     # "always give the path of the script that initiated the process"
-    #     local_mod_path = os.path.dirname(__file__)
-    # except NameError:
-    #     # for inside spyder
-    #     local_mod_path = "/Users/cdesbois/pg/chrisPg/anesthPlot/anesplot/config"
-    # filename = os.path.join(local_mod_path, "recordRc.yaml")
-    # # load onfiguration dico
-    # print(filename)
     if os.path.isfile(cfg_filename):
         with open(cfg_filename, encoding="utf-8") as ymlfile:
             cfg = yaml.safe_load(ymlfile)
@@ -175,33 +116,31 @@ def build_new_paths(record_main_dir: str) -> dict[str, str]:
 def write_configfile(path: dict[str, Any]) -> None:
     """Record the yaml file."""
     config_loc = os.path.join(path["recordMain"], "config")
-    os.chdir(path[config_loc])
+    os.chdir(config_loc)
     with open("recordRc.yaml", "w", encoding="utf-8") as ymlfile:
         yaml.dump(path, ymlfile, default_flow_style=False)
+    print("builded config/recordRc.yaml that contains:")
+    print(f"{'-'*20}")
+    for key, loc in path.items():
+        print(key, loc)
+    print(f"{'-'*20}")
 
 
 def main() -> None:
     """Script execution entry point."""
     # test if paths exists (ie config present)
-    if "paths" not in dir():
-        # get package directory
-        key = "record_main.py"
-        record_main_dir = dlg.choose_file(
-            dirname=os.getcwd(), title=f"select the file '{key}'", filtre="*.py"
-        )
-        if os.path.isfile(record_main_dir):
-            record_main_dir = os.path.dirname(record_main_dir)
-        config_filename = os.path.join(record_main_dir, "config", "recordRc.yaml")
-        paths = read_config(config_filename)
+    record_main_dir, config_filename = get_config_files()
+    paths = read_config(config_filename)
+    print("config file contains:")
+    for key, path in paths.items():
+        print(key, path)
     # paths empty <-> no config_file
     if not paths:
         paths = build_new_paths(record_main_dir)
-    # write config
-    write_configfile(paths)
+        # write config
+        write_configfile(paths)
 
 
 # %%
-
-
 if __name__ == "__main__":
     main()
